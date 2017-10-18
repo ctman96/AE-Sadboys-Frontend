2228b7db04a6edcd16dae52e9c7cd44652099a22
Abhishek <abhishek.mohan@live.com>
2017-10-17 17:35:36 -0700

    Have a working side navigation bar that is able to go to config and print routes

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  actions: {
+    transitionTo: function(param){
+      this.transitionTo(param);
+    },
+    willTransition: function(){
+      this.controller.set('drawerOpen', false);
+    }
+  }
 });
