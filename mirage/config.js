50a36900adb9a08e6521991b0731212dd2e989fa
Cody Newman <cody.newman@alumni.ubc.ca>
2017-11-05 18:32:38 -0800

    Example backend connection
    Create serializer, disable mirage, create an example pulling classifications from the backend

--- mirage/config.js
+++ mirage/config.js
@@ -8,8 +8,8 @@ export default function() {
     Note: these only affect routes defined *after* them!
   */
 
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-   this.namespace = '/api';    // make this `/api`, for example, if your API is namespaced
+   this.urlPrefix = 'http://localhost:8080';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  // this.namespace = '/api';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
   /*
@@ -35,4 +35,9 @@ export default function() {
       ]
     }
   })
+
+  this.get('/classifications', 'example');
+  this.get('/classifications/:id', 'example');
+
+  this.passthrough();
 }
