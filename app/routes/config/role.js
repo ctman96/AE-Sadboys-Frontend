8090206fb9d36f499391926b369972b10ba94b3b
Abhishek <abhishek.mohan@live.com>
2017-11-07 13:27:07 -0800

    Able to display role by id

--- app/routes/config/role.js
+++ app/routes/config/role.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params){
-    return this.get('store').findRecord('roles', params.id);
+    return this.get('store').findRecord('role', params.id);
   }
 });
