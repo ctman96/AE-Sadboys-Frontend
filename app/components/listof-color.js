ca6a001f2bbbecb91e4ef71b4e4094a5c05aea41
Carol Leung <Carol@Carols-MacBook-Air.local>
2017-11-19 20:19:54 -0800

    colours

--- app/components/listof-color.js
+++ app/components/listof-color.js
@@ -1,10 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  ajax: Ember.inject.service(),
+  store: Ember.inject.service(),
   edit: false,
   actions: {
     edit(){
       this.toggleProperty('edit');
+    },
+    delete(){
+      var store = this.get('store');
+      this.get('ajax').request(store.adapterFor('application').host+'/labelcolours/'+this.get('role.id'), {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(jsonapirequest =>{
+        window.location.reload(true);
+      })
+    },
+    update(){
+      var store = this.get('store');
+      this.get('ajax').request(store.adapterFor('application').host+'/labelcolours', {
+        method: 'POST',
+        data: JSON.stringify({
+          key: this.get('colour.key'),
+          colours: this.get('colour.colour'),
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(jsonapirequest =>{
+        window.location.reload(true);
+      })
     }
   }
 });
--- app/controllers/config/location.js
+++ app/controllers/config/location.js
@@ -1,14 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  locations: [
-      {
-        name: "somewhere",
-        users: ['username1','username2','username3']
-      },
-      {
-        name: "anywhere",
-        users: ['person1','person2','person3']
-      }
-    ]
+  ajax: Ember.inject.service(),
+  actions: {
+    create() {
+      this.get('ajax').request(this.store.adapterFor('application').host+'/locations', {
+        method: 'POST',
+        data: JSON.stringify({
+          name: this.get('newLocation')
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(jsonapirequest =>{
+        window.location.reload(true);
+      })
+    }
+  }
 });
