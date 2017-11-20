c55fd2d7821acb3135efb540bf6a0b7d650818cf
Carol Leung <Carol@Carols-MacBook-Air.local>
2017-11-19 17:09:36 -0800

    moved to controller

--- app/controllers/config/role.js
+++ app/controllers/config/role.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  ajax: Ember.inject.service(),
+  actions: {
+    create() {
+      return this.get('ajax').request(this.store.adapterFor('application').host+'/roles', {
+        method: 'POST',
+        data: {
+          name: this.get('newRole')
+        }
+      });
+    }
+  }
 });
--- app/routes/config/role.js
+++ app/routes/config/role.js
@@ -1,18 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  ajax: Ember.inject.service(),
   model(){
     return this.get('store').findAll('role');
-  },
-  actions: {
-    create() {
-      return this.get('ajax').request(this.store.adapterFor('application').host+'/roles', {
-        method: 'POST',
-        data: {
-          name: this.get('newRole')
-        }
-      });
-    }
   }
 });
