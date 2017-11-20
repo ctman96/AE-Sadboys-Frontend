a9783572fbea18f2db0f99ebda2bcf5738cf8f0a
Cody Newman <cody.newman@alumni.ubc.ca>
2017-11-19 22:12:47 -0800

    A lot of improvements

--- app/routes/config.js
+++ app/routes/config.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  actions: {
+    loading(transition, originRoute) {
+      let controller = this.controllerFor('config');
+      controller.set('currentlyLoading', true);
+      transition.promise.finally(function() {
+        controller.set('currentlyLoading', false);
+      });
+    }
+  }
 });
