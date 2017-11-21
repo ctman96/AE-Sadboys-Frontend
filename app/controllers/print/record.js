543fbb44252a7284eca58e5feb4c87bb582727ef
Kiku <kikugumo@protonmail.com>
2017-11-20 20:44:50 -0800

    cleaned record print up

--- app/controllers/print/record.js
+++ app/controllers/print/record.js
@@ -21,11 +21,11 @@ const previousPartLabelYoffset = 20;
 const previousPartXoffset = 80;
 const previousPartYoffset = 20;
 const clientNameLabelXoffset = 12;
-const clientNameLabelYoffset = 30;
-const clientNameXoffset = 40;
-const clientNameYoffset = 30;
+const clientNameLabelYoffset = 27;
+const clientNameXoffset = 35;
+const clientNameYoffset = 27;
 const classificationPathXoffset = 14;
-const classificationPathYoffset = 40;
+const classificationPathYoffset = 37;
 const maxLineLength = 46;
 
 
@@ -34,22 +34,7 @@ export default Ember.Controller.extend({
   selectedRecords: [],
   steps: [],
   actions: {
-    update: function() {
-      const recordsString = localStorage.getItem("recordsToPrint");
-      var records = JSON.parse(recordsString);
-      records = records ? records : [];
-      
-      this.set('records', records);
-    },
-    setData: function() {
-
-        const records = [ { "id": 52, "number": "EDM-2003/002", "title": "Consequatur voluptas soluta in incidunt omnis praesentium illum beatae in voluptate adipisci ipsum accusamus id est", "schedule": { "id": 408, "name": "PUBLICATION - PRODUCTION", "code": "P5.P2.00 ", "years": 1 }, "type": { "id": 3, "name": "Subject", "numberPattern": "KKK-yyyy/ggg", "defaultSchedule": null }, "consignmentCode": "DESTRUCTION CERTIFICATE 2007-001", "state": { "id": 6, "name": "Destroyed" }, "container": null, "location": { "id": 5, "name": "Edmonton", "code": "edm", "locked": false, "users": [] }, "createdAt": 1063584000000, "updatedAt": 1208822400000, "closedAt": 1072828800000, "classifications": [ { "id": 244, "name": "PRODUCTION", "keyword": "F", "updatedAt": 1051999425000 }, { "id": 519, "name": "PUBLICATION", "keyword": "T", "updatedAt": 1051997475000 } ] }, { "id": 53, "number": "EDM-2003/003", "title": "Expedita ea recusandae culpa tempore", "schedule": { "id": 345, "name": "PERSONNEL - RECRUITMENT", "code": "P2.R1.03 ", "years": 0 }, "type": { "id": 3, "name": "Subject", "numberPattern": "KKK-yyyy/ggg", "defaultSchedule": null }, "consignmentCode": "DESTRUCTION CERTIFICATE 2007-001", "state": { "id": 6, "name": "Destroyed" }, "container": null, "location": { "id": 5, "name": "Edmonton", "code": "edm", "locked": false, "users": [] }, "createdAt": 1063584000000, "updatedAt": 1208822400000, "closedAt": 1072828800000, "classifications": [ { "id": 147, "name": "RECRUITMENT", "keyword": "F", "updatedAt": 1051987500000 }, { "id": 250, "name": "PERSONNEL", "keyword": "T", "updatedAt": 1053269080000 } ] },{ "id": 51, "number": "EDM-2003/001", "title": "Laboriosam at sapiente temporibus", "schedule": { "id": 26, "name": "BUSINESS DEVELOPMENT - CONFERENCES", "code": "B1.C3.02 ", "years": 5 }, "type": { "id": 3, "name": "Subject", "numberPattern": "KKK-yyyy/ggg", "defaultSchedule": null }, "consignmentCode": "DESTRUCTION CERTIFICATE 2009-01", "state": { "id": 6, "name": "Destroyed" }, "container": { "id": 24365, "number": "2007/014-EDM", "title": "Quidem et dolorum ut nisi voluptatem voluptatum vel eveniet aspernatur et tempore earum quam maiores", "consignmentCode": "DESTRUCTION CERTIFICATE 2009-01", "createdAt": 1072742400000, "updatedAt": 1360540800000 }, "location": { "id": 5, "name": "Edmonton", "code": "edm", "locked": false, "users": [] }, "createdAt": 1063584000000, "updatedAt": 1238025600000, "closedAt": 1072828800000, "classifications": [ { "id": 14, "name": "CONFERENCES", "keyword": "F", "updatedAt": 1053214494000 }, { "id": 1052, "name": "BUSINESS DEVELOPMENT", "keyword": "T", "updatedAt": 1053972153000 } ] } ];
-        
-        localStorage.setItem("recordsToPrint", JSON.stringify(records));
-      
-    },
     print: function() {
-      
       const steps = [];
       
       var originX = 0;
@@ -63,7 +48,6 @@ export default Ember.Controller.extend({
           recordCount = 0;
           steps.push({addPage: []});
         }
-        
         
         var input = [];
         var classificationPathAndTitle = [];
