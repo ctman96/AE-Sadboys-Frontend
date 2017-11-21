cf405d8e38bce46b913c91c5f29b266bd2c1848d
Abhishek <abhishek.mohan@live.com>
2017-11-20 16:48:47 -0800

    Testing out different users for auth

--- app/authenticators/custom.js
+++ app/authenticators/custom.js
@@ -1,16 +1,17 @@
-import RSVP from 'rsvp';
-import Base from 'ember-simple-auth/authenticators/base';
+import Ember from 'ember';
 
-export default Base.extend({
-  restore(data) {
-    return new RSVP.Promise((resolve, reject) => {
-      return resolve(data);
-    });
-  },
+export default Ember.Component.extend({
+  session: Ember.inject.service('session'),
+  ajax: Ember.inject.service(),
 
-  authenticate(options) {
-    return new RSVP.Promise((resolve, reject) => {
-        resolve(options);
-    });
+  actions: {
+    authenticate: function(){
+      let curUser =  this.get('ajax').request(this.store.adapterFor('application').host+'/users/currentuser');
+      this.get('session').authenticate('authenticator:custom', curUser);
+      this.get('session').set('data.user', curUser);
+    },
+    invalidate: function(){
+      this.get('session').invalidate();
+    }
   }
 });
