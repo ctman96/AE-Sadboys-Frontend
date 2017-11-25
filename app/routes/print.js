15bebdd44ea27eb5172db566a3c9bf804476af13
Cody Newman <cody.newman@alumni.ubc.ca>
2017-11-24 16:38:59 -0800

    Inject session

--- app/routes/print.js
+++ app/routes/print.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service('session'),
   beforeModel: function() {
+
     if (this.get('session.data.authenticated') &&  (this.get('session.data.authenticated.admin') || this.get('session.data.authenticated.rmc')))
     {
       return;
