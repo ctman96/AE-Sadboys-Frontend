ce262c8aeab5f5f39b1610b19b8b7cc6cf141553
Cody Newman <cody.newman@alumni.ubc.ca>
2017-11-24 16:52:05 -0800

    Logout and fix transitions

--- app/components/page-toolbar.js
+++ app/components/page-toolbar.js
@@ -3,16 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   session: Ember.inject.service('session'),
   ajax: Ember.inject.service(),
+  router: Ember.inject.service('-routing'),
 
   actions: {
     authenticate: function(){
       let curUser =  this.get('ajax').request(this.store.adapterFor('application').host+'/users/currentuser');
       this.get('session').authenticate('authenticator:custom', curUser);
       this.get('session').set('data.user', curUser);
-      this.transitionTo('home');
+      this.get('router').transitionTo('home');
     },
     invalidate: function(){
       this.get('session').invalidate();
+      this.get('router').transitionTo('home');
     }
   }
 });
